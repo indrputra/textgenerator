4d342533feabbd29369245094e236f2640762d61
INDRputra <49543162+indrputra@users.noreply.github.com>
2023-08-10 19:04:46 +0800

    Add files via upload

---
+++ sizes.js
@@ -0,0 +1,13 @@
+
+  $('.font-slider input[type="range"]').on("input change", function() {
+  var newSize = $(this).val(),
+    defaultSize = $("input.form-control").css("font-size"),
+    minSize = 12,
+    maxSize = 50;
+
+  if (newSize <= maxSize && newSize >= minSize) {
+    $("input.form-control").css("font-size", newSize + "px");
+
+    //localStorage.setItem("__ACCESSIBILITY__font_size", newSize);
+  }
+});
