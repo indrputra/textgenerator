d56425e5e8ebf5615dda71202e5d0b66d68a1895
INDRputra <49543162+indrputra@users.noreply.github.com>
2023-08-14 15:19:00 +0800

    Update sizes.js

--- sizes.js
+++ sizes.js
@@ -2,7 +2,7 @@
   $('.font-slider input[type="range"]').on("input change", function() {
   var newSize = $(this).val(),
     defaultSize = $("input.form-control").css("font-size"),
-    minSize = 12,
+    minSize = 13,
     maxSize = 50;
 
   if (newSize <= maxSize && newSize >= minSize) {
